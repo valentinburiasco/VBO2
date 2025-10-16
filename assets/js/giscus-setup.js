ef1d2365c1aa098c6752772e0f17ecf4c1f0be5a
valentinburiasco <valentinburiasco@users.noreply.github.com>
2025-10-16 13:08:39 +0000

    Deploying to gh-pages from @ valentinburiasco/VBO2@6e1dcba848222bc2ed1779a2d2805fffe30e1004 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "alshedivat/al-folio",
+    "data-repo": "valentinburiasco/VBO2",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
